ab51d0884709cb68bb0c6d26a95e2d1c04b5a4a9
mwteusz <240759@edu.p.lodz.pl>
2023-05-18 18:22:19 +0200

    usuniecie zbednego pola w Map

--- src/main/java/com/skarpeta/skarpeciarzegame/Map.java
+++ src/main/java/com/skarpeta/skarpeciarzegame/Map.java
@@ -6,11 +6,9 @@ import static com.skarpeta.skarpeciarzegame.Catana.fieldColors;
 public class Map {
     public Field[][] board;
     private final int BOARD_SIZE;
-    private final int ROWS_COUNT;
     Map(int size) {
         board = new Field[size][size];
         BOARD_SIZE = size;
-        ROWS_COUNT = (int) Math.ceil((double) BOARD_SIZE/3);
         for(int y = 0; y< BOARD_SIZE; y++) {
             for (int x = 0; x < BOARD_SIZE; x++) {
                 Point point = new Point(x,y);
