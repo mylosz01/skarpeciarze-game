3863bbad96c786e58eed77880d77d7de0b80270c
mwteusz <240759@edu.p.lodz.pl>
2023-05-20 18:01:54 +0200

    dodanie zooma pod scrollem

--- src/main/java/com/skarpeta/skarpeciarzegame/Catana.java
+++ src/main/java/com/skarpeta/skarpeciarzegame/Catana.java
@@ -2,13 +2,16 @@ package com.skarpeta.skarpeciarzegame;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Catana extends Application {
-    public static final int BOARD_SIZE = 30;
-    public static final double FIELD_WIDTH = 1000.0/BOARD_SIZE;
+    public static final int BOARD_SIZE = 60;
+    public static final double FIELD_WIDTH = 50;
+    private double currentScale = 1.0;
+    private static final double ZOOM_FACTOR = 1.1;
 
     static final ColorPalette palette= new ColorPalette(Color.valueOf("4BC87F"),Color.valueOf("6EAFF7"));
 
@@ -19,11 +22,29 @@ public class Catana extends Application {
         StackPane root = new StackPane(map);
         Scene scene = new Scene(root);
         scene.setFill(palette.blue);
+        scene.setOnScroll(this::handleScroll);
         stage.setTitle("catana");
         stage.setScene(scene);
         stage.show();
 
     }
+
+    private void handleScroll(ScrollEvent scrollEvent) {
+        double delta = scrollEvent.getDeltaY();
+
+        if (delta > 0) {
+            // Zoom in
+            map.setScaleX(currentScale * ZOOM_FACTOR);
+            map.setScaleY(currentScale * ZOOM_FACTOR);
+            currentScale *= ZOOM_FACTOR;
+        } else if (delta < 0) {
+            // Zoom out
+            map.setScaleX(currentScale / ZOOM_FACTOR);
+            map.setScaleY(currentScale / ZOOM_FACTOR);
+            currentScale /= ZOOM_FACTOR;
+        }
+    }
+
     public static void main(String[] args) {
         launch();
     }
