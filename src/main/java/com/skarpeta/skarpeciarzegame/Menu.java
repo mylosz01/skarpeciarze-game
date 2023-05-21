76f99ce7eacf8a0c857201d1ac2b5976dd650e53
mylosz01 <240840@edu.p.lodz.pl>
2023-05-21 15:11:54 +0200

    Dodanie obrazkow do menu

--- src/main/java/com/skarpeta/skarpeciarzegame/Menu.java
+++ src/main/java/com/skarpeta/skarpeciarzegame/Menu.java
@@ -2,10 +2,16 @@ package com.skarpeta.skarpeciarzegame;
 
 import javafx.application.*;
 import javafx.geometry.Pos;
+import javafx.scene.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 import javafx.stage.*;
+
+import java.io.InputStream;
 
 public class Menu extends Application {
 
@@ -21,17 +27,34 @@ public class Menu extends Application {
         layoutMenu.setAlignment(Pos.CENTER);
         layoutMenu.setSpacing(20);
 
-        Button btnStartGame = new Button("Start Game");
-        Button btnJoinGame = new Button("Join Game");
-        Button btnExit = new Button("Exit");
+        Label logo = new Label("Katan");
+        logo.setStyle("-fx-font: 16px \"Serif\";");
 
-        layoutMenu.getChildren().addAll(btnStartGame,btnJoinGame,btnExit);
+        Button btnStartGame = new Button("START GAME");
+        btnStartGame.setStyle("-fx-font: 16px \"Serif\";");
+
+        Button btnJoinGame = new Button("JOIN GAME");
+        btnJoinGame.setStyle("-fx-font: 16px \"Serif\";");
+
+        Button btnExit = new Button("EXIT");
+        btnExit.setStyle("-fx-font: 16px \"Serif\";");
+
+        layoutMenu.getChildren().addAll(logo,btnStartGame,btnJoinGame,btnExit);
+        //layoutMenu.setBackground(new Background(new BackgroundImage(new Image("D:\\Java\\skarpeciarze-game\\assets\\background2.jpg"),BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.CENTER,new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, true))));
 
         menuScene = new Scene(layoutMenu);
 
+        InputStream imageSrc = this.getClass().getResourceAsStream("/assets/background1.jpg");
+        System.out.println(imageSrc);
+        //menuScene.setCursor(new ImageCursor(new Image("D:\\Java\\skarpeciarze-game\\src\\main\\assets\\kursor.png")));
+
         menu.setScene(menuScene);
-        menu.setMinWidth(600);
-        menu.setMinHeight(400);
+        menu.setMinWidth(700);
+        menu.setMinHeight(500);
+
+        menu.setMaxWidth(1000);
+        menu.setMaxHeight(700);
+
         menu.setTitle("Sockware game Catan");
         menu.show();
     }
