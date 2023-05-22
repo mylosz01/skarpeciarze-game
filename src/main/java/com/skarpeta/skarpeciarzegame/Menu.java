27a0187a908ae612608d680f6ee77a22110ff237
mylosz01 <240840@edu.p.lodz.pl>
2023-05-22 13:41:48 +0200

    Drobna poprawa ukladu menu

--- src/main/java/com/skarpeta/skarpeciarzegame/Menu.java
+++ src/main/java/com/skarpeta/skarpeciarzegame/Menu.java
@@ -25,9 +25,12 @@ public class Menu extends Application {
 
         VBox layoutMenu = new VBox();
         layoutMenu.setAlignment(Pos.CENTER);
-        layoutMenu.setSpacing(20);
+        layoutMenu.setSpacing(40);
+
+        Group logoPane = new Group();
 
         ImageView logo = new ImageView(ImageManager.getImage("logoGame"));
+        logoPane.getChildren().add(logo);
 
         MenuButton btnStartGame = new MenuButton("btnStart");
 
@@ -35,8 +38,12 @@ public class Menu extends Application {
 
         MenuButton btnExit = new MenuButton("btnQuit");
 
+        VBox btnLayout = new VBox();
+        btnLayout.setSpacing(10);
+        btnLayout.setAlignment(Pos.CENTER);
+        btnLayout.getChildren().addAll(btnStartGame.getImageView(),btnJoinGame.getImageView(),btnExit.getImageView());
 
-        layoutMenu.getChildren().addAll(logo,btnStartGame.getImageView(),btnJoinGame.getImageView(),btnExit.getImageView());
+        layoutMenu.getChildren().addAll(logoPane,btnLayout);
         layoutMenu.setBackground(new Background(new BackgroundImage(new Image("file:src/main/resources/images/background1.jpg"),BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.CENTER,new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, true))));
 
         menuScene = new Scene(layoutMenu);
@@ -56,11 +63,6 @@ public class Menu extends Application {
 
     public static void main(String[] args) throws InterruptedException {
         launch();
-    }
-
-    private void mouseEntered(MouseEvent event, ImageView btn) {
-        btn.setScaleX(1.3);
-        btn.setScaleY(1.3);
     }
 
 }
