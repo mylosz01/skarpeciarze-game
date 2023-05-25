fa569099fac1f0e1e61f587e4ff6dcd81b2ecd41
mwteusz <240759@edu.p.lodz.pl>
2023-05-25 13:19:30 +0200

    zmiana grafiki w menu

--- src/main/java/com/skarpeta/skarpeciarzegame/MenuButton.java
+++ src/main/java/com/skarpeta/skarpeciarzegame/MenuButton.java
@@ -1,37 +1,20 @@
 package com.skarpeta.skarpeciarzegame;
 
 import com.skarpeta.skarpeciarzegame.tools.ImageManager;
-
-import javafx.application.*;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.*;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.image.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.*;
-import javafx.stage.*;
+
 
 /** Przycisk z wybraną grafiką */
-public class MenuButton {
-
-    private final ImageView btnImage;
-
+public class MenuButton extends Asset{
+    ImageView hover_texture = new ImageView(ImageManager.getImage("hover.png",128,128));
     /** Tworzenie przycisku z grafiką o nazwie pliku imageName (wraz z rozszerzeniem)*/
     MenuButton(String imageName){
-
-        btnImage = new ImageView(ImageManager.getImage(imageName,128,128));
-
-        btnImage.setOnMouseEntered(e -> {btnImage.setScaleX(1.3); btnImage.setScaleY(1.3);});
-        btnImage.setOnMouseExited(e -> {btnImage.setScaleX(1); btnImage.setScaleY(1);});
-        btnImage.setOnMouseClicked(e -> System.out.println("CLikcked!!"));
+        super(new ImageView(ImageManager.getImage(imageName,128,128)));
+        setOnMouseEntered(e -> getChildren().add(hover_texture));
+        setOnMouseExited(e -> getChildren().remove(hover_texture));
+        setOnMouseClicked(e -> System.out.println("CLikcked!!"));
     }
 
     /** Zwracanie grafiki przycisku jako ImageView */
-    public ImageView getImageView(){
-        return btnImage;
-    }
 
 }
