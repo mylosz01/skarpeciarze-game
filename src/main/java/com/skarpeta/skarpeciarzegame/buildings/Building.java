10e3b48712d62feb3af65b70b638b75525566b0c
mwteusz <240759@edu.p.lodz.pl>
2023-06-01 22:22:35 +0200

    ujednolicenie konstruktorow klas dziedziczacych po Asset

--- src/main/java/com/skarpeta/skarpeciarzegame/buildings/Building.java
+++ src/main/java/com/skarpeta/skarpeciarzegame/buildings/Building.java
@@ -1,11 +1,12 @@
 package com.skarpeta.skarpeciarzegame.buildings;
 
 import com.skarpeta.skarpeciarzegame.Asset;
+import com.skarpeta.skarpeciarzegame.tools.ImageManager;
 import javafx.scene.image.ImageView;
 /** Building definiuje budynki budowane przez graczy */
 public abstract class Building extends Asset {
-    public Building(ImageView texture) {
-        super(texture);
+    public Building(String textureName) {
+        super(new ImageView(ImageManager.getImage(textureName+".png",128,128)));
         align();
     }
     /** align() służy budynkom do przyjęcia pozycji w prawym górnym rogu pola Field w którym się znajduje*/
